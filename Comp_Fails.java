01514873f29fe8ecf1a76177fdf3e5a6a343fbca
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-09-21 21:17:30 +0100

    Add files via upload

---
+++ Comp_Fails.java
@@ -0,0 +1,13 @@
+
+public class Comp_Fails {
+	public static void main(String args[]) {
+		int x = 0, y = 10;
+		try {
+			y /=x;
+			}
+		
+		//System.out.println("/ by 0");
+		  catch(Exception e) {
+			  System.out.println("error");
+		   }
+	   }  }
