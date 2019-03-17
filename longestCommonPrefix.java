075253e0eafcf94a4474ce6c1ff390eb7a1c3061
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2019-03-17 00:53:11 +0000

    Update longestCommonPrefix.java

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -1,12 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0 || strs == null) {
+        if(strs.length == 0 || strs == null) { // if there is only 1 string or the array elements are null
             return "";
         }
         
         for(int i = 0; i < strs[0].length(); i++) {
             for(int j = 1; j < strs.length; j++) {
-                if(i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)){
+                if(i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)){ // i increments until != condition is true
                     return strs[0].substring(0, i);
                 }
             }
